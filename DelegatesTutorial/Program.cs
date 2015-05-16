ec04cc1559cfa5421ec4cd2fc90d48e789fff5ac
timmyers1978 <timmyers1978@gmail.com>
2015-05-16 08:46:17 -0600

    Fully functional at this point. Added Delegate and then used it twice. Once to print the names that start with "A" and once to print the names that end with "N".

--- DelegatesTutorial/Program.cs
+++ DelegatesTutorial/Program.cs
@@ -8,10 +8,50 @@ namespace DelegatesTutorial
 {
     class Program
     {
-        static delegate bool StringProcessor(string s);
+        public delegate bool FunctionForString(string s);
+
+
+        public static string[] PerformOperationOnStringArray(string[] myStrings, FunctionForString myFunction)
+        {
+            System.Collections.ArrayList myList = new System.Collections.ArrayList();
+            foreach(string s in myStrings)
+            {
+                if (myFunction(s))
+                {
+                    myList.Add(s);
+                }
+            }
+            return (string[])myList.ToArray(typeof(string));
+        }
+
+
+        public static bool StartsWithA(string s)
+        {
+            return s.StartsWith("A");
+        }
+
+        public static bool EndsWithN(string s)
+        {
+            return s.EndsWith("n");
+        }
 
         static void Main(string[] args)
         {
+            string[] myStrings = { "Adam", "Alan", "Bob", "Steve", "Jim", "Aiden" };
+            string[] stringsA = PerformOperationOnStringArray(myStrings, StartsWithA);
+
+            string[] stringsN = PerformOperationOnStringArray(myStrings, EndsWithN);
+
+            foreach(string s in stringsN)
+            {
+                Console.WriteLine("Names that Start With the Letter A: " + s);
+            }
+
+            foreach (string s in stringsA)
+            {
+                Console.WriteLine("Names that end with the letter N: " + s);
+            }
+            Console.ReadLine();
         }
     }
 }
